ba7878c7e881d4a788496648649b206077735068
MariaHRCastro <mariahribeiro@protonmail.com>
2022-11-19 16:54:19 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -21,11 +21,17 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-        console.log("Teste");
-        axios.get(`${BASE_URL}/sales`).then(response => {
+
+        const dmin = minDate.toISOString().slice(0,10);
+
+        const dmax = maxDate.toISOString().slice(0,10);
+
+        console.log(dmin);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(response => {
             setSales(response.data.content);
         })
-    }, [])
+    }, [minDate,maxDate])
 
     return (
         <div className="dsmeta-card">
